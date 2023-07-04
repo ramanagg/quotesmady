6e76d8be3fa68c352c7ebbff0f8b6e89bd54b8ee
Raman Aggarwal <rama9agg+github@gmail.com>
2023-07-04 21:43:31 +0530

    quote api added

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,8 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+Route::get('/quote', function () {
+    $quotes=config('quotes');
+    $retuenQuoteKey=array_rand($quotes);
+    return response($quotes[$retuenQuoteKey]['quote'],200);
 });
